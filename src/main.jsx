df8cb395e63c8801d2a4ac2933825cad39c2f159
Timothy Mills <redroyalty910@users.noreply.github.com>
2026-08-05 18:34:32 -0400

    pages ready

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react" // helps React identify certain development problems, better for debug purposes
 import { createRoot } from "react-dom/client" // createRoot connects our REACT application to the regular HTML page
-import { BrowserRouter } from "react-router" // BrowserRouter gives all components within it access to routing features
+import { HashRouter } from "react-router" // HashRouter gives all components within it access to routing features and prevents pages from breaking when somebody refreshes
 import "./index.css" // global CSS
 import App from "./App.jsx" // imports the app component from app.jsx
 
 createRoot(document.getElementById("root")).render( // creates the REACT application, .render creates a REACT root using that HTML element
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
